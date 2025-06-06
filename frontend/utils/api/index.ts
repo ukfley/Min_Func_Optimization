cb831debe823aa16950f7182bae4dcabb606296d
ukfley <103734427+ukfley@users.noreply.github.com>
2025-06-06 22:20:16 +0300

    Delete frontend/utils directory

--- frontend/utils/api/index.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/api/index.ts
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // Адреса вашого сервера
-  withCredentials: true,
-});
-
-export default axiosInstance;
